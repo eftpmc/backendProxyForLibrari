a7b0e21d5e4ad8034169b62d3a94c7481a97c9ee
SaturnEFTPMC <zackhhi@gmail.com>
2023-10-05 16:44:18 -0400

    cors

--- api/proxy.js
+++ api/proxy.js
@@ -2,6 +2,18 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
+const allowedOrigins = ["https://librari-one.vercel.app", "http://localhost:3000"];
+
+app.use((req, res, next) => {
+  const origin = req.get('origin'); // Get the origin of the incoming request
+
+  if (allowedOrigins.includes(origin)) {
+    next();  // If the origin is allowed, proceed
+  } else {
+    res.status(403).send("Forbidden: Not Allowed Origin");  // If not, block the request
+  }
+});
+
 app.get('/api/proxy', async (req, res) => {
   try {
     const url = req.query.url;
